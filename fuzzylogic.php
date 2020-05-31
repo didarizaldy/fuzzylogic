d35a2812f62db8586c835bbf450c097162406f47
did <63806329+didarizaldy@users.noreply.github.com>
2020-05-31 23:06:25 +0700

    Create fuzzylogic.php

---
+++ fuzzylogic.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Input Data Fuzzy</title>
+</head>
+<body style="text-align: center;">
+<h1>Fuzzy</h1>
+<br>
+<br>
+<form action="" method="POST">
+ <input type="number" name="input" placeholder="Input Angka Disini">
+ <input type="submit" name="submit" value="Submit"> 
+</form>
+<h3>Nilai</h3>
+<br>
+<?php include 'hasil.php'; ?>
+
+</body>
+</html>
